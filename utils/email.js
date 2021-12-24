234d9ab7ec6145ff1e3bc24ff2bb5162bc673fdb
Mardochée <MardoxheeLuviki@gmail.com>
2021-12-24 16:53:25 +0100

    fix: email system

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const sendgridTransport = require("nodemailer-sendgrid-transport");
+const sendgridTransport = require("nodemailer-sendgrid");
 
 const sendMail = async (options) => {
   // 1) Create a transporter to send email
