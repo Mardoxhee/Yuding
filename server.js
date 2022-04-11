6b832c022030af289c2020df50f80d07f2fb55e8
Mardochée <MardoxheeLuviki@gmail.com>
2022-04-11 18:28:44 +0100

    fix:port

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ mongoose
 
 //  Port configuration
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server running on port${port}`);
+  console.log(`server running on port ${port}`);
 });
